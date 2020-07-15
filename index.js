722d15affd8fd6a522a08140150bbf085b99e61a
roukell <62136980+roukell@users.noreply.github.com>
2020-07-15 11:31:28 +1000

    added questions

--- index.js
+++ index.js
@@ -1,16 +1,66 @@
-// array of questions for user
-const questions = [
+const inquirer = require("inquirer");
+const fs = require("fs");
 
+
+// array of questions for user
+const questions = [{
+    type: "input",
+    message: "What is your GitHub username?",
+    name: "username"
+  },
+  {
+    type: "input",
+    message: "What is your email address?",
+    name: "email"
+  },
+  {
+    type: "input",
+    message: "What is your project's name?",
+    name: "project"
+  },
+  {
+    type: "input",
+    message: "Please write a short description of your project?",
+    name: "description"
+  },
+  {
+    type: "input",
+    message: "What kind of license should your project have?",
+    name: "license"
+  },
+  {
+    type: "input",
+    message: "What command should be run to install dependencies?",
+    name: "commandDep"
+  },
+  {
+    type: "input",
+    message: "What command should be run to run tests?",
+    name: "commandRun"
+  },{
+    type: "input",
+    message: "What does the user need to know about using the repo?",
+    name: "repoUse"
+  },
+  {
+    type: "input",
+    message: "What does the user need to know about contributing to the repo?",
+    name: "repoContribute"
+  }
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+
 }
 
 // function to initialize program
 function init() {
-
+   
+    
 }
 
 // function call to initialize program
 init();
+
+
